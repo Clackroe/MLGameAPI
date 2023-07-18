8f5b1bcce4d7c93506348ba5cdce26a3db264c44
Xander Cole <acole67@uncc.edu>
2023-07-17 21:39:46 -0400

    Updated tslint

--- src/db.ts
+++ src/db.ts
@@ -1,2 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
+
+export function testFunc() {
+  console.log("testFunc");
+}
--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,15 @@ import express from "express";
 const app = express();
 const port = 3000; // default port to listen
 
+import { testFunc } from "./db";
 // define a route handler for the default home page
 app.get("/", (req, res) => {
   res.send("Hello world!");
+  testFunc();
 });
 
 // start the Express server
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
-  console.log(`server started at http://localhost:${port}`);
+  console.log(`server started on http://localhost:${port}`);
 });
