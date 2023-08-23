3207085a247c2c308e362944b601b4f84f885377
Xander Cole <acole67@uncc.edu>
2023-08-23 11:57:37 -0400

    Updated bash script

--- DockerJobTests/run.sh
+++ DockerJobTests/run.sh
@@ -1,9 +1,10 @@
-sudo docker run -it --name testSocc2      
---mount type=bind,source="$(pwd)"/unity-volume,target=/unity-volume 
--p 5005:5005           
--p 6006:6006            
-clackroe/test-soccer:latest            
-/unity-volume/test-soccer-twos.x86_64            
---env=/unity-volume/SoccerTwos.yaml            
---train            
---run-id=test-run
+
+sudo docker run -it --name testSocc5  \
+--mount type=bind,source="$(pwd)"/unity-volume,target=/unity-volume  \
+-p 5005:5005  \
+-p 6006:6006  \
+clackroe/test-soccer:latest \
+mlagents-learn /unity-volume/SoccerTwos.yaml \
+--env=unity-volume/test-soccer-twos.x86_64 \
+--train \
+--run-id=test-run --force --debug --no-graphics
