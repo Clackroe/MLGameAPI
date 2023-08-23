ea7c61cb4e36a8d610f41cb095772917bae4842f
Xander Cole <acole67@uncc.edu>
2023-08-23 12:02:43 -0400

    Update bash

--- DockerJobTests/run.sh
+++ DockerJobTests/run.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
 
-sudo docker run -it --name testSocc5  \
---mount type=bind,source="$(pwd)"/unity-volume,target=/unity-volume  \
--p 5005:5005  \
--p 6006:6006  \
-clackroe/test-soccer:latest \
-mlagents-learn /unity-volume/SoccerTwos.yaml \
---env=unity-volume/test-soccer-twos.x86_64 \
---train \
---run-id=test-run --force --debug --no-graphics
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <container_name>"
+  exit 1
+fi
+
+container_name="$1"
+
+sudo docker run -d -it --name "$container_name" \
+  --mount type=bind,source="$(pwd)"/unity-volume,target=/unity-volume \
+  -p 6006:6006 \
+  clackroe/test-soccer:latest \
+  mlagents-learn /unity-volume/Walker.yaml  --run-id=testRun --no-graphics --force \
+  --env=/unity-volume/walker-exec.x86_64
