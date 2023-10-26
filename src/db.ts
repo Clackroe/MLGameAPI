c211ef17d47162ea83ef6d72915cf825d2b446e1
Erin Woodard <erin@stiegleredtech.org>
2023-10-26 11:19:28 -0400

    conditional for rank calcs

--- src/db.ts
+++ src/db.ts
@@ -601,13 +601,13 @@ export async function getEquationMatchesByUserId(id: string) {
 // -------------------------------- UserInEquationMatch --------------------------------
 //Erin - if EquationMatch.type is ranked, update ratings/scores, if not then just update status of match
 export async function updateEquationMatchUserMuSigma(eqMatchID: string) {
-  const matchType = (await getEquationMatchById(eqMatchID)).type;
-  // const isRanked = matchType == "Ranked";
-  // if (isRanked) {
+  const match = await getEquationMatchById(eqMatchID);
+  const isNotRanked = match.type === "Practice";
     try {
+      if(isNotRanked) {
       const ratings = await getUserMatchRatings(eqMatchID);
-
       await updateUserScores(ratings);
+      };
 
       await prisma.equationMatch.update({
         where: {
@@ -622,23 +622,7 @@ export async function updateEquationMatchUserMuSigma(eqMatchID: string) {
       console.error("Error updating EquationMatch User Mu Sigma:", error);
       throw new Error("Failed to update EquationMatch User Mu Sigma.");
     }
-  // } else {
-    // try {
-  //     await prisma.equationMatch.update({
-  //       where: {
-  //         id: eqMatchID,
-  //       },
-  //       data: {
-  //         ended: new Date(),
-  //         status: "FINISHED",
-  //       },
-  //     });
-  //   } catch (error) {
-  //     console.error("Error updating unranked EquationMatch", error);
-  //     throw new Error("Failed to update unranked EquationMatch.");
-  //   }
-  // }
-}
+  };
 
 export async function addUserToEquationMatch(
   matchID: string,
